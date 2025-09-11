fd8463e1c42187087f2b9410f92d9a71cf87adcf
THJUtah <kyle@thehoneyjarhome.com>
2025-09-10 18:59:29 -0600

    Update processor.py

--- processor.py
+++ processor.py
@@ -3,7 +3,7 @@ import argparse
 from pypdf import PdfReader, PdfWriter, Transformation
 
 POINTS_PER_INCH = 72.0
-PAGE_W_IN = 4.02
+PAGE_W_IN = 4.06
 PAGE_H_IN = 2.1875
 GAP_IN     = 0.18
 SCALE_PCT  = 0.005
